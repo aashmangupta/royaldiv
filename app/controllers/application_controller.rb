badad9d73049937f9a52313b639fe008cf9765b8
AashmanGupta <aashmangupta@eab.com>
2021-06-30 21:15:00 -0700

    Authentication fully functional now

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  require 'pry'
+  before_action :configure_permitted_params, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_params
+    devise_parameter_sanitizer.permit(:update, keys: %i[first_name last_name url])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name])
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_27_191801) do
+ActiveRecord::Schema.define(version: 2021_07_01_000118) do
 
   create_table "accounts", force: :cascade do |t|
     t.string "first_name", default: "", null: false
@@ -18,7 +18,6 @@ ActiveRecord::Schema.define(version: 2021_06_27_191801) do
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "url"
-    t.integer "user_id"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
